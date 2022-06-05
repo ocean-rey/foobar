03e63ac842c70cb8ec3a3184e98a213a0b897c46
ocean-rey <x7flux@gmail.com>
2022-06-05 17:32:27 +0300

    almost working

--- src/KnightNavigator.java
+++ src/KnightNavigator.java
@@ -1,3 +1,4 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -13,13 +14,52 @@ public class KnightNavigator {
         scanner.close();
     }
 
+    // bfs
     public static int getMinMoves(int src, int dest) {
+        ArrayDeque<Integer> q = new ArrayDeque<Integer>();
+        ArrayDeque<Integer> parents = new ArrayDeque<Integer>();
+        ArrayDeque<Integer> explored = new ArrayDeque<Integer>();
+        if (src == dest) {
+            return 0;
+        }
         int min = 0;
-        int[] targets = getLegalMoves(src);
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 8; j++) {
-                if (targets[j] == dest) {
-                    min = i;
+        int currentParent = 1;
+        q.add(src);
+        parents.push(-1); // indicates root
+        while (!q.isEmpty()) {
+            int verify = q.remove();
+            if (verify != dest) {
+                explored.push(verify);
+                int[] nextMoves = getLegalMoves(verify);
+                System.out.println("from " + verify + " legal moves are " + Arrays.toString(nextMoves));
+                for (int i = 0; i < nextMoves.length; i++) {
+                    if (nextMoves[i] != -1 && !explored.contains(nextMoves[i])) {
+                        System.out.println("adding " + nextMoves[i] + " to q");
+                        System.out.println("adding " + verify + " to parents");
+                        q.add(nextMoves[i]);
+                        parents.push(verify);
+                    }
+                }
+            } else {
+                System.out.println(verify + " is " + dest);
+                // count the parents to the root
+                while (!q.isEmpty()) {
+                    System.out.println("q size = " + q.size());
+                    System.out.println("parents size = " + parents.size());
+                    q.remove();
+                    currentParent = parents.pop();
+                }
+                while (currentParent != src) {
+                    min++;
+                    int backTrack = -1;
+                    while (backTrack != currentParent) {
+                        backTrack = explored.pop();
+                        if (backTrack == currentParent) {
+                            currentParent = parents.pop();
+                        } else {
+                            parents.pop();
+                        }
+                    }
                 }
             }
         }
@@ -28,12 +68,12 @@ public class KnightNavigator {
 
     static int[] getLegalMoves(int src) {
         int[] legalMoves = new int[8]; // there cannot be more than 8 legal moves at a time
+        Arrays.fill(legalMoves, -1);
         int currentIndex = 0;
         for (int i = -2; i <= 2; i++) {
             if (i == 0) {
                 continue;
             }
-            System.out.println(i);
             try {
                 int landedRow = rowMove(src, i);
                 for (int j = -1; j <= 1; j++) {
@@ -44,14 +84,12 @@ public class KnightNavigator {
                     try {
                         destination = colMove(landedRow, j * ((Math.abs(i) % 2) + 1));
                     } catch (Exception e) {
-                        // System.out.println(e);
                         continue;
                     }
                     legalMoves[currentIndex] = destination;
                     currentIndex++;
                 }
             } catch (Exception e) {
-                // System.out.println(e);
                 continue;
             }
         }
@@ -70,7 +108,7 @@ public class KnightNavigator {
         int landsAt = src + colsToMove;
         int currentRow = src / 8;
         int landedRow = landsAt / 8;
-        if (currentRow != landedRow) {
+        if (currentRow != landedRow || landsAt > 63 || landsAt < 0) {
             throw new Exception("Invalid col move from " + src + " to " + colsToMove + " cols");
         }
         return landsAt;
