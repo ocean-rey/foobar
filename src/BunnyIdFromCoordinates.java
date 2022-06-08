13d87d672d9481da129833c5be6d6131bad98343
ocean-rey <x7flux@gmail.com>
2022-06-08 18:55:00 +0300

    idk how to be more optimal

--- src/BunnyIdFromCoordinates.java
+++ src/BunnyIdFromCoordinates.java
@@ -1,17 +1,30 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class BunnyIdFromCoordinates {
-    final static int MAX = 14;
 
     public static void main(String args[]) {
-        
+
         System.out.println("(3,2) => " + solution(3, 2));
-        System.out.println("(5, 10) => "+ solution(5, 10));
+        System.out.println("(5, 10) => " + solution(5, 10));
+        System.out.println("(1, 50) => " + solution(1, 50));
+        //System.out.println("(99999, 99999) => " + solution(99999, 99999));
     }
 
     public static String solution(long x, long y) {
+        // the smallest possible triangle where points x / y are on the hypotenuse.
+        // slope = 1 because base and height are the same.
+        // slope = hieght / base = 1
+        // hieght = base * slope
+        // a^2 + b^2 = c
+        // c = x + y
+        // (b*slope)^2 + (b*slope)^2  = x + y
+        // math stuff yay
+        int max = (int) Math.round((int) (x + y)-2 / Math.sqrt(2));
+        System.out.println(max);
         // given triangle
         // | 7
         // | 4 8
@@ -22,31 +35,26 @@ public class BunnyIdFromCoordinates {
         // | 2 3
         // | 4 5 6
         // | 7 8 9 10
-        // after reflecting and rotating
-        // the latter is easier to compute so we will use that one and then do some
-        // other stuff later
-        long[][] floydsTriangle = floydsTriangle();
-        // we can cheat now
-        // each diagonal in our floyds triangle translates to a row in the weird
-        // triangle google wants me to make
-        int zeros = 0;
-        for (int i = 0; i < floydsTriangle[(int) x - 1].length; i++) {
-            if (floydsTriangle[(int) x - 1][i] == 0) {
-                zeros++;
-            }
-        }
-        return Long.toString(floydsTriangle[(int) x - 1][Math.abs((int) y - (MAX - zeros))]);
+        // x = 0 {1, 2, 4, 7} for both, only one is reversed. holds for all values of x
+        List<List<Long>> listVer = floydsTriangleList(max);
+        return Long.toString(listVer.get((int) x - 1).get((int) (y - 1)));
     }
 
-    static long[][] floydsTriangle() {
+    static List<List<Long>> floydsTriangleList(int MAX) {
         long value = 1;
-        long[][] floydsTriangle = new long[MAX][MAX];
+        // long[][] floydsTriangle = new long[MAX][MAX];
+        List<List<Long>> floydsTriangleList = new ArrayList<List<Long>>();
+        for (int i = 0; i < MAX; i++) {
+            // create empty lists until max
+            floydsTriangleList.add(new ArrayList<Long>());
+        }
         for (int yAxis = MAX; yAxis > 0; yAxis--) {
             for (int xAxis = 1; xAxis <= MAX - yAxis + 1; xAxis++) {
-                floydsTriangle[xAxis - 1][yAxis - 1] = value;
+                floydsTriangleList.get(xAxis - 1).add(value);
+                // floydsTriangle[xAxis - 1][yAxis - 1] = value;
                 value++;
             }
         }
-        return floydsTriangle;
+        return floydsTriangleList;
     }
 }
