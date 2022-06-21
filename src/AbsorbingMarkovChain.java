1bd960e318f2459c30cbacfdf6efc435d4eea7e8
ocean-rey <x7flux@gmail.com>
2022-06-21 15:17:04 +0300

    todo: absorbing markov chain

---
+++ src/AbsorbingMarkovChain.java
@@ -0,0 +1,15 @@
+public class AbsorbingMarkovChain {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] transformToMarkovMatrix(int[][] input) {
+        int[][] matrix = new int[input.length][input[0].length];
+        return matrix;
+    }
+
+    int[] solution(int[][] m) {
+        int[] answer = new int[m.length - 1];
+        return answer;
+    }
+}
